70f33d98024d658ce67effe6c709008a74c49fcc
Manuel Bieh <github@manuelbieh.de>
2011-09-29 17:52:51 +0200

    New temporary translation logger added.

--- Includes/Functions.inc.php
+++ Includes/Functions.inc.php
@@ -9,6 +9,8 @@ if(!function_exists('__')) {
 	function __($string, $locale=NULL) {
 
 		$translationsFile = Application_Base::getCoreDir() . 'i18n/Strings.txt';
+		$translationsFile = Application_Base::getCoreDir() . 'i18n/Strings2.txt';
+
 		if(file_exists($translationsFile)) {
 
 			ob_start();
@@ -16,11 +18,21 @@ if(!function_exists('__')) {
 			$c = ob_get_contents();
 			ob_end_clean();
 
+			$strings = explode('/* STRING END */' . "\n", $c);
+
+			$newTranslationString = '$_lang[\'' . str_replace("'", "\'", $string) . '\'] = \'\';';
+			if(!in_array($newTranslationString, $strings)) {
+				$strings[] = $newTranslationString;
+			}
+			$newContent = join("/* STRING END */\n", $strings);
+			file_put_contents($translationsFile, $newContent);
+/*
 			eval('$t = ' . $c . ';');
 			array_push($t, $string);
 			$t = array_unique($t);
 			$v = var_export($t, true);
 			file_put_contents($translationsFile, $v);
+*/
 
 		}
 
