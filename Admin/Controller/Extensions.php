48481475ed50dbd5bf8f3f127d82b5f53b22d388
Manuel Bieh <github@manuelbieh.de>
2011-11-05 14:20:34 +0100

    Extension management improvements

--- Admin/Controller/Extensions.php
+++ Admin/Controller/Extensions.php
@@ -9,6 +9,9 @@ class Admin_Controller_Extensions extends Controller_Frontend {
 		$this->view		= $this->app->objectManager->get('Application_View');
 		$this->view->loadHTML('templates/index.html');
 
+		$this->extGateway	= new Model_Extension_Gateway_PDO($this->app->objectManager->get('Datastore'));
+		$this->extMapper	= new Model_Extension_Mapper($this->extGateway);
+
 		$navi = $this->app->createView();
 		$navi->loadHTML("templates/main/navi.html");
 
@@ -29,18 +32,77 @@ class Admin_Controller_Extensions extends Controller_Frontend {
 
 	public function manage() {
 
-		$extensionGateway	= new Model_Extension_Gateway_PDO($this->app->objectManager->get('Datastore'));
-		$extensionMapper	= new Model_Extension_Mapper($extensionGateway);
+#		$this->app->extensions()->buildIndex();
 
-#		$ext = $extensionMapper->find('SystemTweaks', new Model_Extension);
-		$this->app->extensions()->buildIndex();
+		$core				= $this->app->getCoreDir();
+		$project			= $this->app->getProjectDir();
+
+		$dirs				= explode('/', rtrim($project, '/'));
+		$project			= end($dirs) == 'Admin' ? realpath(rtrim($project, '/') . '/..') : $project;
+
+		$coreFiles			= glob(realpath($core) . '/Extensions/*.xml');
+		$projectFiles		= glob($project . '/Extensions/*.xml');
+		$files				= array_unique(array_merge($coreFiles, $projectFiles));
+
+		$xml = new Modules_XML();
+
+		foreach($files AS $extMeta) {
+
+			$extKey = basename($extMeta, '.xml');
+			$ext = $this->extMapper->find($extKey, new Model_Extension);
+
+			$xml->load($extMeta);
+
+			$name = $xml->XPath()->query("//extension/@name");
+			$desc = $xml->XPath()->query("//extension/@desc");
+			$deps = $xml->XPath()->query("//extension/@deps");
+			$core = $xml->XPath()->query("//extension/@core");
+			$icon = $xml->XPath()->query("//extension/@icon");
+
+			$extData = array(
+				'extKey'=>$extKey, 
+				'name'=>$name->item(0)->textContent, 
+				'desc'=>$desc->item(0)->textContent, 
+				'deps'=>$deps->item(0)->textContent, 
+				'core'=>$core->item(0)->textContent, 
+				'icon'=>$icon->item(0)->textContent
+			);
+
+			if($ext != NULL) {
+				$active[] = $extData;
+			} else {
+				$inactive[] = $extData;
+			}
+
+		}
+
+		$subview = $this->app->createView();
+		$subview->loadHTML('templates/extensions/manage.html');
+
+		$subview->data['active'] = $active;
+		$subview->data['inactive'] = $inactive;
+
+		$this->view->addSubview('main', $subview);
 
 
+	}
+
+	public function activate($extKey) {
+		$ext = $this->extMapper->find($extKey, new Model_Extension);
+	}
+
+	public function deactivate($extKey) {
+		$ext = $this->extMapper->find($extKey, new Model_Extension);
 	}
 
 	public function browse() {
 	
 	}
 
+	public function settings($extension_key) {
+
+		$settings = new Modules_XML();
+		
+	}
 
 }
