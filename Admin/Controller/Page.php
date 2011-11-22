b6fcd01ccb5d9b6f76676019b19576f2301f2ba1
Manuel Bieh <github@manuelbieh.de>
2011-11-22 17:12:55 +0100

    Made pages sortable!

--- Admin/Controller/Page.php
+++ Admin/Controller/Page.php
@@ -27,26 +27,67 @@ class Admin_Controller_Page extends Controller_Frontend {
 
 	public function view() {
 
-		$allPages	= $this->pageMapper->fetchAll();
+		$pageData = $this->getPageStructure();
 
 		$subview	= $this->app->createView();
 		$subview->loadHTML('templates/page/view.html');
 
-		$subview->data['pages'] = $allPages;
-
-		foreach($allPages AS $page => $data) {
-
-			$data->cssclass = '';
-			$data->cssclass .= $data->active == 1 ? ' active ':' inactive ';
-			$data->cssclass .= $data->childcount > 0 ? ' haschildren ':'';
-			$pages[$data->parent_page_id][$data->page_id] = $data;
-
-		}
-
-		$subview->data['nested_pages'] = $pages;
+		$subview->data['pages'] = $pageData['flatArray'];
+		$subview->data['nested_pages'] = $pageData['parentChild'];
 
 		$this->view->addSubview('main', $subview);
 
+	}
+
+	public function reorder() {
+
+		$pageData = $this->getPageStructure();
+		$factor = 1024;
+
+		foreach($pageData['flatArray'] AS $num => $data) {
+			$oldStructure[$data->page_id]['parent_page_id'] = $data->parent_page_id;
+			$oldStructure[$data->page_id]['sort'] = $data->sort;
+		}
+
+
+		// make structure parent->page first so the pages are ordered correctly!
+		foreach($_POST['page'] AS $page_id => $parent_page_id) {
+
+			$parent_page_id = (int) $parent_page_id;
+
+			if(!isset($counter[$parent_page_id])) {
+				$counter[$parent_page_id] = 0;
+			} else {
+				$counter[$parent_page_id] += 1;
+			}
+
+			if((int) $oldStructure[$page_id]['sort'] !== (int) $counter[$parent_page_id] * $factor) {
+				$newStructure[$page_id]['sort'] = $counter[$parent_page_id] * $factor;
+			}
+
+			if((int) $oldStructure[$page_id]['parent_page_id'] !== (int) $parent_page_id) {
+				$newStructure[$page_id]['parent_page_id'] = (int) $parent_page_id;
+				$newStructure[$page_id]['sort'] = $counter[$parent_page_id] * $factor;
+			}
+
+
+		}
+
+
+		if(is_array($newStructure)) {
+
+			foreach($newStructure AS $page_id => $properties) {
+
+				$page = new Model_Page();
+				$page->page_id = $page_id;
+				$page->parent_page_id = $properties['parent_page_id'];
+				$page->sort = $properties['sort'];
+				$this->pageMapper->save($page);
+
+			}
+
+		}
+		
 	}
 
 	public function create($parent_page_id=0) {
@@ -67,6 +108,33 @@ class Admin_Controller_Page extends Controller_Frontend {
 
 	public function sort($type, $id, $order) {
 		
+	}
+
+	protected function getPageStructure() {
+
+		$allPages	= $this->pageMapper->fetchAll();
+
+		if(is_array($allPages)) {
+
+			$pages = array();
+
+			foreach($allPages AS $page => $data) {
+
+				$data->cssclass = '';
+				$data->cssclass .= $data->active == 1 ? ' active ':' inactive ';
+				$data->cssclass .= $data->childcount > 0 ? ' haschildren ':'';
+				$pages[$data->parent_page_id][$data->page_id] = $data;
+
+			}
+
+		}
+
+		$ret = array();
+		$ret['flatArray'] = $allPages;
+		$ret['parentChild'] = $pages;
+
+		return $ret;
+
 	}
 
 }
