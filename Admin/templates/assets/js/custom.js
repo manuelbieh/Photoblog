474f36ffef1954a5eb65272601018f7354aa6baf
Manuel Bieh <github@manuelbieh.de>
2011-11-26 01:53:10 +0100

    Eliminated (currently) unnecessary index controller Added ShowComments Setting CSS Tuning Corrected routing Moved ajax navigation to an external file

--- Admin/templates/assets/js/custom.js
+++ Admin/templates/assets/js/custom.js
@@ -1,3 +1,108 @@
+$(window).on('popstate', function(event) {
+
+	$('ul#menu li ul li').removeClass('active');
+
+	$.ajax({
+
+		url: window.location,
+		type: 'get',
+		data: 'ajax=true',
+		success: function(content) {
+
+			$('#content').html(content).css({opacity: 1});
+			$('ul#menu li ul li').removeClass('active');
+			$('#' + event.originalEvent.state.active).addClass('active');
+			$('.contentspinner').remove();
+
+		},
+
+		error: function() {
+			$('#content').css({opacity: 1});
+			$('.contentspinner').remove();
+		}
+
+	});
+
+});
+
+$(function() {
+
+	// fold inactive menu items
+	$('ul#menu li:not(.active) ul').hide();
+
+	// prevent links whose parent lis have nested lists
+	$('ul#menu > li:has(> ul) > a').bind('click', function(e) {
+		e.preventDefault();
+		$(this).parent().find('ul').slideToggle('fast');
+	});
+
+	$('#sidebar .resizebar').bind('click', function() {
+
+		if($(this).parent().hasClass('closed')) {
+			$('#content').stop().animate({left: '300px'});
+			$('#sidebar').stop().animate({width: '300px'});
+		} else {
+			$('#sidebar').stop().animate({width: '8px'});
+			$('#content').stop().animate({left: '8px'});
+		}
+
+		$(this).parent().toggleClass('closed');
+
+	});
+
+	// Awesome ajax menu load stuff. Currently doesnt work with forms as good as it could
+	$('ul#menu li ul li a').bind('click', function(e) {
+
+		e.preventDefault();
+
+		$('#content').css({opacity: 0.3});
+
+		$('body').append('<div class="contentspinner"></div>');
+
+		$$ = $(this);
+		$.ajax({
+			url: $$.attr('href'),
+			type: 'get',
+			data: 'ajax=true',
+			success: function(content) {
+				$('#content').html(content).css({opacity: 1});
+				$('ul#menu li ul li').removeClass('active');
+				$$.parent('li').addClass('active');
+
+				var generateTempId = function() {
+					return 'item_' + Math.ceil(Math.random() * 12000);
+				}
+
+				var activeItem = $('ul#menu li ul li.active');
+
+				if(!activeItem.attr('id')) {
+
+					var tempId = generateTempId();
+
+					while($('#' + tempId).length) {
+						tempId = generateTempId();
+					}
+					activeItem.attr('id', tempId);
+
+				}
+
+				history.pushState({active: tempId, text: activeItem.text()}, null, $$.attr('href'));
+
+				$('.contentspinner').remove();
+				console.log(history);
+			},
+			error: function() {
+				$('#content').css({opacity: 1});
+				$('.contentspinner').remove();
+			}
+		});
+
+		return false;
+
+	});
+
+});
+
 function ajaxLoad(link) {
 
 	$.ajax({
@@ -5,9 +110,30 @@ function ajaxLoad(link) {
 		type: 'get',
 		data: 'ajax=true',
 		success: function(content) {
+
 			$('#content').html(content).css({opacity: 1});
-			history.pushState({}, null, link);
+
+			var generateTempId = function() {
+				return 'item_' + Math.ceil(Math.random() * 12000);
+			}
+
+			var activeItem = $('ul#menu li ul li.active');
+
+			if(!activeItem.attr('id')) {
+
+				var tempId = generateTempId();
+
+				while($('#' + tempId).length) {
+					tempId = generateTempId();
+				}
+				activeItem.attr('id', tempId);
+
+			}
+
+			history.pushState({active: tempId, text: activeItem.text()}, null, link);
+
 		},
+
 		error: function() {
 			$('#content').css({opacity: 1});
 			$('.contentspinner').remove();
--- Controller/Photo.php
+++ Controller/Photo.php
@@ -20,12 +20,27 @@ class Controller_Photo extends Controller_Frontend {
 
 	}
 
+
+	public function index($link='page', $param1=NULL) {
+
+		$allPhotos			= $this->photoMapper->fetchAll();
+		$this->showPhotos($allPhotos, $link, $param1);
+
+	}
+
+
+	public function tag($link='tag', $tag, $param1=NULL) {
+
+		$allPhotos = $this->photoMapper->findByTag($tag);
+		$this->showPhotos($allPhotos, $link . '/' . $tag, $param1, array('photosPerPage'=>5));
+
+	}
+
+
 	public function view($cleanTitle) {
 
 		$photo				= $this->photoMapper->fetchWhere(array('clean_title'=>$cleanTitle));
 		$photo				= $photo[0];
-
-		$commentController	= new Controller_Comment($this->app);
 
 		$subview = new Application_View_Theme();
 
@@ -42,6 +57,7 @@ class Controller_Photo extends Controller_Frontend {
 
 			$subview->loadHTML('photo/view.html');
 			if((int) $photo->allow_comments === 1) {
+				$commentController	= new Controller_Comment($this->app);
 				$subview->assign('commentform', $commentController->commentForm($photo->photo_id));
 			} else {
 				$subview->assign('commentform', __('Comments are closed.'));
@@ -73,7 +89,7 @@ class Controller_Photo extends Controller_Frontend {
 	}
 
 
-	public function overview($filter='all', $param1=0, $order='desc') {
+	public function overview($page=0, $order='desc') {
 
 		$allPhotos			= $this->photoMapper->fetchAll();
 		$allPhotosReverse	= is_array($allPhotos) ? array_reverse($allPhotos) : array();
@@ -81,11 +97,11 @@ class Controller_Photo extends Controller_Frontend {
 		$subview = new Application_View_Theme();
 		$subview->loadHTML('photo/overview.html');
 
-		$itemsPerPage	= 30;
+		$itemsPerPage	= 20;
 
 		$totalItems		= count($allPhotos);
 
-		$page				= (int) $param1 < 0 ? 0 : (int) $param1;
+		$page				= (int) $page < 0 ? 0 : (int) $page;
 		$offset				= $page * $itemsPerPage;
 
 		$subview->data['offset'] = (int) $offset;
@@ -104,8 +120,135 @@ class Controller_Photo extends Controller_Frontend {
 			}
 		}
 
+		$pagina = new Modules_Pagination;
+		$pagina->usePages = true;
+		$pagina->setLink($this->app->getBaseURL() . "overview/")->setItemsPerPage($itemsPerPage)->setItemsTotal($totalItems)->currentPageNum($page);
+		if($order == 'asc') {
+			$pagina->setParams('/' . $order);
+		}
+		$subview->data['pagination'] = $pagina->render();
+
+
 		$this->view->addSubview('main', $subview);
 
 	}
+
+
+	private function showPhotos($allPhotos, $link=NULL, $param1=NULL, $options=array()) {
+
+		$photosPerPage		= (int) Application_Settings::get('//theme//photosPerPage');
+		if((int) $options['photosPerPage'] !== 0) {
+			$photosPerPage = (int) $options['photosPerPage'];
+		}
+		$photosPerPage		= $photosPerPage < 1 ? 1 : $photosPerPage;
+
+		$totalPhotos		= count($allPhotos);
+
+		$allPhotosReverse	= is_array($allPhotos) ? array_reverse($allPhotos) : array();
+		$page				= (int) $param1 < 0 ? 0 : (int) $param1;
+		$offset				= $page * $photosPerPage;
+
+		$subview = new Application_View_Theme();
+		$subview->loadHTML('photo/index.html');
+
+		$showComments = Application_Settings::get('//theme/layout/showCommentsOnIndex');
+
+		$subview->data['showComments'] = $showComments;
+
+		for($i = $offset; $i < $offset + $photosPerPage; $i++) {
+
+			if($allPhotosReverse[$i]->datenum > date('YmdHis')) {
+
+				continue;
+
+			} else {
+
+				$currentPhoto = $allPhotosReverse[$i];
+
+				if(Modules_Filesys::isFile($this->app->getProjectDir() . 'uploads/web/' . $currentPhoto->web_name)) {
+
+					$image = new Modules_Image($this->app->getProjectDir() . 'uploads/web/' . $currentPhoto->web_name);
+
+					$currentPhoto->width = $image->getImageWidth();
+					$currentPhoto->height = $image->getImageHeight();
+					$currentPhoto->photographer = $this->userMapper->find($currentPhoto->user_id, new Model_User);
+
+					$maxWidth	= $maxWidth == NULL || $currentPhoto->width > $maxWidth ? $currentPhoto->width : $maxWidth;
+					$maxHeight	= $maxHeight == NULL || $currentPhoto->height > $maxHeight ? $currentPhoto->height : $maxHeight;
+
+					$currentPhoto->photographer	= $this->userMapper->find($currentPhoto->user_id, new Model_User);
+
+					if($currentPhoto->tags != '') {
+
+						$tags = explode(',', $currentPhoto->tags);
+
+						foreach($tags AS $j => $tag) {
+							$tag = htmlentities($tag, ENT_NOQUOTES, 'UTF-8');
+							$tags[$j] = '<a href="' . $this->app->getBaseURL() . 'tag/' . $tag .'">' . $tag . '</a>';						
+						}
+
+						$currentPhoto->taglinks = $tags;
+
+					} else {
+
+						$currentPhoto->taglinks = array();
+
+					}
+
+					if($showComments == 1) {
+
+						if((int) $currentPhoto->allow_comments === 1) {
+							$commentController = new Controller_Comment($this->app);
+							$subview->assign('commentform', $commentController->commentForm($currentPhoto->photo_id));
+						} else {
+							$subview->assign('commentform', __('Comments are closed.'));
+						}
+
+						$currentPhoto->comments	= $this->commentMapper->findByPhoto($currentPhoto->photo_id);
+
+					}
+
+					$subview->data['photos'][] = $currentPhoto;
+
+				}
+
+			}
+
+		}
+
+		if((int) $totalPhotos > 0) {
+
+			$this->view->data['maxWidth'] = $maxWidth;
+			$this->view->data['maxHeight'] = $maxHeight;
+
+			if( (($page+1) * $photosPerPage) < $totalPhotos) {
+				$subview->data['prevLink'] = $this->app->getBaseURL() . $link . '/' . ((int) $page+1);
+			} else {
+				$subview->data['prevLink'] = '';
+			}
+
+			if(!($page < 1)) {
+				$subview->data['nextLink'] = $this->app->getBaseURL() . $link . '/' . ((int) $page-1);
+			} else {
+				$subview->data['nextLink'] = '';
+			}
+
+			$pagina = new Modules_Pagination;
+			$pagina->setUsePages(true)
+					->setAtLast(3)
+					->setAtLeast(3)
+					->setLink($this->app->getBaseURL() . $link . "/")
+					->setItemsPerPage($photosPerPage)
+					->setItemsTotal($totalPhotos)
+					->currentPageNum($page);
+			$subview->data['pagination'] = $pagina->render();
+
+		}
+
+		$this->view->addSubview('main', $subview);
+
+
+	}
+
 
 }
